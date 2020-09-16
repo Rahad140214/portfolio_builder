95e8650c3e5a8f70eb6c471781d8cdb3fd29de2f
rahadmannan@gmail.com <rahad.mannan@oscillosoft.net>
2020-09-16 21:13:13 +0600

    Registration page added

---
+++ registration.js
@@ -0,0 +1,13 @@
+function handleValueChange(e){
+    var element= e.target.name;
+    var value= e.target.value;
+    document.getElementById(element).innerHTML = value;//e.target.name is the value of registration
+}
+
+function handleImageUpload(e){
+    var reader = new FileReader();
+    reader.onload = function(readerEvent) {
+        document.getElementById(e.target.name).src = readerEvent.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]); 
+}
